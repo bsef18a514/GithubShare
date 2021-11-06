40cab9afad9940ce940eaa04a84521e95886e914
bsef18a514 <91655339+bsef18a514@users.noreply.github.com>
2021-11-06 16:14:53 +0500

    Github link added

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,11 @@
-package com.example.formdatashare;
+package com.example.githublink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void githubshare(View v)
+    {
+        String url = "https://github.com/bsef18a514?tab=repositories";
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+
+
+    }
+
 }
